2479f9441ae8c5c02fed8b41e613ca55d4809242
Ilyazbek2 <101692898+Ilyazbek2@users.noreply.github.com>
2025-12-28 13:25:42 +0100

    Create create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    department VARCHAR(50),
+    month VARCHAR(15),
+    work_life_balance INT
+);
+
+INSERT INTO employees VALUES
+(1,'Admin Offices','January',3),
+(2,'Admin Offices','February',3),
+(3,'Admin Offices','December',2),
+(4,'IT','January',4),
+(5,'IT','December',3);
